b0817992607b7591febb18348ff202b6c052647b
kestefo <kestefo@ravaconsulting.com.pe>
2022-09-21 00:05:40 +0000

    Primera vista sapui5

--- pedidoventa/webapp/controller/Main.controller.js
+++ pedidoventa/webapp/controller/Main.controller.js
@@ -13,24 +13,47 @@ sap.ui.define([
             this.oRouter.getTarget("Main").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
 
             this.frgIdAddPedido = "frgIdAddPedido";
+            this.frgIdDetailPedido = "frgIdDetailPedido";
+            this.frgIdEditContact = "frgIdEditContact";
             
             console.log("init")
         },
         handleRouteMatched: function(){
-            Promise.all([this.validateUser()]).then(async values => {
+            Promise.all([]).then(async values => {
 				this.oModelPedidoVenta = this.getModel("oModelPedidoVenta");
                 this.oModelPedidoVenta.setProperty("/AddSelectUser", models.JsonUser());
-                var oUser = this.validateUser();
-                this.oModelPedidoVenta.setProperty("/User", oUser);
-                this.setFragment("_dialogAddPedido", this.frgIdAddPedido, "AddPedido", this);
+                this.oModelPedidoVenta.setProperty("/User", models.JsonUserLoged());
+                this.oModelPedidoVenta.setProperty("/PedidosCreados", models.JsonPedidos());
+                this.oModelPedidoVenta.setProperty("/DetailSelectFuerzaVenta", models.JsonFuerzaVenta());
+                this.oModelPedidoVenta.setProperty("/DetailSelectPuntoVenta", models.JsonPuntoVenta());
+                this.oModelPedidoVenta.setProperty("/DetailSelectDireccion", models.JsonDirecciones());
+                this.oModelPedidoVenta.setProperty("/DetailSelectCondPago", models.JsonCondPago());
 			}).catch(function (oError) {
 				sap.ui.core.BusyIndicator.hide(0);
 			});
+        },
+        _onPressAddPedido: function(){
+            this.setFragment("_dialogAddPedido", this.frgIdAddPedido, "AddPedido", this);
         },
         _onPressClose: function (oEvent) {
 			var oSource = oEvent.getSource();
 			var sCustom = oSource.data("custom");
             oSource.getParent().close();
-		}
+		},
+        _onAcceptPedido: function(){
+            this._dialogAddPedido.close();
+            this.setFragment("_dialogDetailPedido", this.frgIdDetailPedido, "DetailPedido", this);
+        },
+        _onPressEditPedido: function(){
+            this.setFragment("_dialogDetailPedido", this.frgIdDetailPedido, "DetailPedido", this);
+        },
+        _onPressEditContact: function(){
+            this.setFragment("_dialogEditContact", this.frgIdEditContact, "EditContact", this);
+        },
+        _onPressAddProducto: function(){
+            this.oRouter.navTo("RouteLaunchpadCreateNotific", {
+                app: "LaunchpadCreateNotific"
+            });
+        }
     });
 });
--- pedidoventa/webapp/model/models.js
+++ pedidoventa/webapp/model/models.js
@@ -31,6 +31,95 @@ sap.ui.define([
                     }
                 ]
                 return oModel;
+            },
+            JsonUserLoged: function () {
+                var oModel = {
+                    "name": "kestefo",
+                    "nameDescription": "Kassiel Estefo Tume"
+                }
+                return oModel;
+            },
+            JsonPedidos: function () {
+                var oModel = [
+                    {
+                        "pedido": "",
+                        "fecha": "20/09/2022",
+                        "vencimiento": "20/09/2022",
+                        "tipodoc": "70599323",
+                        "razonsocial": "SUPERMERCADOS PERUANOS SA",
+                        "importe": "250,000,000",
+                        "estado": "Pendiente Creación",
+                        "icon": "sap-icon://pending",
+                        "estate": "Warning"
+                    },
+                    {
+                        "pedido": "O123-588",
+                        "fecha": "20/09/2022",
+                        "vencimiento": "21/09/2022",
+                        "tipodoc": "70599325",
+                        "razonsocial": "HIPERMERCADO TOTTUS ORIENTE SA",
+                        "importe": "250,000,000",
+                        "estado": "Creado",
+                        "icon": "sap-icon://create",
+                        "estate": "Success"
+                    },
+                    {
+                        "pedido": "O123-589",
+                        "fecha": "20/09/2022",
+                        "vencimiento": "20/09/2022",
+                        "tipodoc": "70599324",
+                        "razonsocial": "CORPORACION BOTICAS PERU SA SA",
+                        "importe": "250,000,000",
+                        "estado": "Cerrado",
+                        "icon": "sap-icon://decline",
+                        "estate": "None"
+                    }
+                ]
+                return oModel;
+            },
+            JsonFuerzaVenta: function () {
+                var oModel = [
+                    {
+                        "key": "01",
+                        "desc": "Placenta life"
+                    }
+                ]
+                return oModel;
+            },
+            JsonPuntoVenta: function () {
+                var oModel = [
+                    {
+                        "key": "01",
+                        "desc": "Centro de Distribución de Villa"
+                    }
+                ]
+                return oModel;
+            },
+            JsonDirecciones: function () {
+                var oModel = [
+                    {
+                        "key": "01",
+                        "desc": "Av. DEFENSOR DEL MORRO No. 1277 URB. PANTANOS DE VILLA [CHORRILLOS LIMA-LIMA-PERU]"
+                    },
+                    {
+                        "key": "02",
+                        "desc": "Av. DEFENSOR DEL MORRO No. 1278 URB. PANTANOS DE VILLA [CHORRILLOS LIMA-LIMA-PERU]"
+                    }
+                ]
+                return oModel;
+            },
+            JsonCondPago: function () {
+                var oModel = [
+                    {
+                        "key": "01",
+                        "desc": "Pago a 90 días"
+                    },
+                    {
+                        "key": "02",
+                        "desc": "Pago a 60 días"
+                    }
+                ]
+                return oModel;
             }
     };
 });
